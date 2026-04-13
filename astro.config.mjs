858a7a2ed5685c4a3a6c75e79f08f7390ce5b617
baterdenets <bbbatuk99@gmail.com>
2026-04-13 16:50:07 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://orinux.space",
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
